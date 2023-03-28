8906f96e4111456cca63e283994da4846e63e155
sangitgreenwich <68804474+sangitgreenwich@users.noreply.github.com>
2023-03-28 14:40:17 +0700

    Convert Nextjs project to use TypeScript

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,7 +7,11 @@ import Link from 'next/link';
 const name = 'Le Sang';
 export const siteTitle = 'Next.js Blog Website';
 
-export default function Layout({ children, home }) {
+export default function Layout({ children, home }: 
+  { 
+    children: React.ReactNode 
+    home?: boolean
+  }) {
   return (
     <div className={styles.container}>
       <Head>
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,8 +3,9 @@ import Head from "next/head";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Date from '../../components/date';
 import utilStyles from '../../styles/utils.module.css'
+import { GetStaticProps, GetStaticPaths } from 'next'
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds();
   return {
     paths,
@@ -12,8 +13,8 @@ export async function getStaticPaths() {
   }
 }
 
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params?.id as string);
 
   return {
     props: {
@@ -22,7 +23,15 @@ export async function getStaticProps({ params }) {
   };
 }
 
-export default function Post({ postData }) {
+export default function Post({
+  postData
+}: {
+  postData: {
+    title: string
+    date: string
+    contentHtml: string
+  }
+}) {
   return (
     <Layout>
       <Head>
@@ -36,5 +45,5 @@ export default function Post({ postData }) {
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
     </Layout>
-  );
+  )
 }
